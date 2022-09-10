55f600ab3978a42da3e32a4e6e76a98b235d2006
balckwidow <76630152+balckwidow@users.noreply.github.com>
2022-09-10 22:36:34 +0300

    t

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,28 +1,32 @@
 import {useState,useEffect} from 'react'
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
 const Cart = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(JSON.parse(localStorage.getItem("Order")));
   let total = 0;
+ 
+  const navigate = useNavigate()
   useEffect(() => {
     setData(JSON.parse(localStorage.getItem("Order")))
-},[])
-
+  },[])
   const removeItem = (e) => {
     let id = e.target.id;
     let list = data;
     list.splice(id,1);
-    console.log(list)
     localStorage.setItem("Order", JSON.stringify(list))
-    setData(list)
   }
- 
+ const handleCheckout=()=>{
+  navigate("/pen")
+ }
   return (
-    <div className='mx-2 '>
-       <div className='mt-1 bg-white '>
-        <div className=" max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-           <h2 className="text-2xl font-bold tracking-tight text-gray-700  ">Shopping Cart</h2>
-           <div className="mt-6 grid grid-cols-1 gap-y- gap-x-6">
-      { 
+   
+    <div className='mx-2 lg:mx-0 lg:w-full'>
+       <div className='mt-1 bg-white lg:w-full'>
+        <div className="py-4 px-4 sm:py-24 sm:px-6  lg:flex lg:flex-col  lg:items-center lg:px-8 lg:w-full  ">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-700 lg:text-center lg:text-4xlxl ">Shopping Cart</h2>
+          {data.length===0&&<p className='mt-6 text-md text-gray-500 font-bold lg:text-2xl '>No Items Found</p>}
+           <div className="mt-6 grid grid-cols-1 gap-x-6 self-center max-w-3xl lg:w-full">
+      { data&&
         data.map((val,index) => {
           total += (parseInt(val.price)*parseInt(val.amount));
         return(
@@ -30,8 +34,8 @@ const Cart = () => {
             
             <div className="group h-fit flex gap-4" >
               
-              <div className='min-h-min overflow-hidden rounded-md  group-hover:opacity-75 lg:aspect-none lg:h-80'>
-                      <img src={val.image} alt={val.name} className="h-40 object-cover object-center" />
+              <div className='min-h-min overflow-hidden rounded-md  group-hover:opacity-75 lg:aspect-0 '>
+                      <img src={val.image} alt={val.name} className="h-40 object-fit object-center lg:h-60" />
                    </div>
                    <div className='flex flex-col w-fit gap-1'>
                      <h3 className=' text-sm font-semibold text-gray-800 md:text-md lg:text-lg'>
@@ -50,14 +54,14 @@ const Cart = () => {
                  )
                } )}
           </div>
-          <div className='mx-auto block w-full'>
-            <div className='mt-4 flex items-center justify-between font-bold text-md text-gray-700 md:text-md lg:text-lg'>
+          <div className='mx-auto block w-full max-w-3xl'>
+            <div className='mt-4 flex items-center justify-between font-bold text-md text-gray-700 md:text-md lg:text-lg '>
               <p>Subtotal</p>
               <p>LBP {total}</p>
             </div>
-            <p className='text-gray-500'>Shipping will be calculated at checkout.</p>
+            <p className='mt-4 text-gray-500'>Shipping will be calculated at checkout.</p>
           </div>
-          <button className='mt-6 py-4 bg-blue-600 text-white font-medium w-full rounded-md md:text-md lg:text-lg'>Checkout</button>
+          <button className='mt-6 py-4 bg-blue-600 text-white font-medium w-full max-w-3xl rounded-md md:text-md lg:text-lg' onClick={handleCheckout}>Checkout</button>
         </div>
     </div>
   </div>
--- src/Pages/PenList.js
+++ src/Pages/PenList.js
@@ -25,7 +25,7 @@ const PenList = () => {
   console.log(data)
   return (
     <div className='mt-1 bg-white'>
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl  px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
            <h2 className="text-2xl font-bold tracking-tight text-gray-900">Pen</h2>
            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {data &&
